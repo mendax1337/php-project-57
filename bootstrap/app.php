2c092179be557706e1802166ff9c58d7b46ffd73
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-08-16 13:37:12 +0300

    add InvalidSignature logging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Sentry\Laravel\Integration;
+use Illuminate\Routing\Exceptions\InvalidSignatureException;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,8 +24,21 @@ return Application::configure(basePath: dirname(__DIR__))
             | \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
         );
     })
-
     ->withExceptions(function (Exceptions $exceptions) {
+        // Интеграция с Sentry
         Integration::handles($exceptions);
+
+        // Логируем ошибки подписи
+        $exceptions->report(function (InvalidSignatureException $e) {
+            Log::warning('Invalid signed URL', [
+                'full_url' => request()->fullUrl(),
+                'method'   => request()->method(),
+                'host'     => request()->getHost(),
+                'scheme'   => request()->getScheme(),
+                'port'     => request()->getPort(),
+                'app_url'  => config('app.url'),
+                'user_id'  => optional(request()->user())->id,
+            ]);
+        });
     })
     ->create();
