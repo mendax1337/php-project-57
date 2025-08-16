8ae75d41557f0b954bd943becdc3942815cc3d2d
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-08-16 18:27:12 +0300

    php stan & linter fix

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\TaskStatusFactory;
 
 class TaskStatus extends Model
 {
+    /** @use HasFactory<TaskStatusFactory> */
     use HasFactory;
 
     protected $fillable = ['name'];
--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use App\Models\TaskStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends Factory<TaskStatus>
+ */
 class TaskStatusFactory extends Factory
 {
     protected $model = TaskStatus::class;
