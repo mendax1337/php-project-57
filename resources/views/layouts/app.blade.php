38da8292d450ba40b8bbffe61346904693448c46
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-08-19 09:00:14 +0300

    fix app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,21 +7,15 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans antialiased bg-gray-100">
-<div class="min-h-screen bg-gray-100">
+<body class="antialiased bg-gray-50">
+<div class="min-h-screen">
     @include('layouts.navigation')
 
-    @isset($header)
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-    @endisset
-
-    <main class="mx-auto max-w-7xl p-6 lg:p-8">
-        @include('flash::message')
-        {{ $slot }}
+    <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {{-- флеши, если нужны глобально --}}
+        @include('flash-message')
+        {{-- сюда отрисуется контент страниц --}}
+        @yield('content')
     </main>
 </div>
 </body>
